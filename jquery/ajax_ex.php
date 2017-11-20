f25ab1d8b1de33b8a18cea137982da95e44bc104
Myeong Lee <deeper.myeong.lee@gmail.com>
2017-11-20 13:56:58 -0500

    exercise + assignment 4

--- jquery/ajax_ex.php
+++ jquery/ajax_ex.php
@@ -21,6 +21,12 @@
 	#square:hover{
 		cursor: pointer;
 	}
+
+	.person {
+		background: lightgreen;
+		height: 20px;
+		width: 100px;
+	}
 	
 </style>
 <script>
@@ -29,30 +35,46 @@ $(document).ready(function(){
 
 	var sentence = "default";
 
-	$("#square").click(function(){
+	// $("#square").click(function(){
+
+	// 	var request1 = $.ajax({
+	// 		url: "ajax_test.php",
+	// 		data: {name: "John Smith"},
+	// 		type: "POST"			
+	// 	});
+
+	
+	// 	$.when(request1.done(function(data, status){
+	// 		sentence = data;
+	// 		$("#square").text(sentence).css("color", "blue");
+	// 	})).then(function(){	
+	// 		var request2 = $.ajax({
+	// 			url: "ajax_answer.php",
+	// 			data: {data: sentence},
+	// 			type: "POST"
+	// 		})		
+	// 		request2.done(function(data, status){
+	// 			// display the returned data below the square
+	// 			console.log(data);	
+	// 			// your code goes here	
+	// 			$("#square").text(data).css("color", "red");
+	// 		});	
+	// 	});
+
+	// });
+
+	$(".person").click(function(e){
+		var target = $(this).html();
 
 		var request1 = $.ajax({
 			url: "ajax_test.php",
-			data: {name: "John Smith"},
+			data: {name: target},
 			type: "POST"			
 		});
 
-	
-		$.when(request1.done(function(data, status){
-			sentence = data;
-			$("#square").text(sentence).css("color", "blue");
-		})).then(function(){	
-			var request2 = $.ajax({
-				url: "ajax_answer.php",
-				data: {data: sentence},
-				type: "POST"
-			})		
-			request2.done(function(data, status){
-				// display the returned data below the square
-				console.log(data);	
-				// your code goes here	
-				$("#square").text(data).css("color", "red");
-			});	
+		request1.done(function(data, status){
+			
+			$("#square").html(data);
 		});
 
 	});
@@ -67,7 +89,10 @@ $(document).ready(function(){
 
 <body>
 	
-	<div id="square"></div>
+	<div id="square"></div> 
+	<div id="john" class="person">john</div>
+	<div id="mary" class="person">mary</div>
+	<div id="steve" class="person">steve</div>
 	
 </body>
 </html>
--- jquery/ajax_test.php
+++ jquery/ajax_test.php
@@ -6,7 +6,8 @@ $post = $_POST["name"];
 if ($get){
 	echo "Hello, " . $get . ". Received Data from _GET";
 } else if ($post){
-	echo "Hello, " . $post . ". Received Data from _POST";
+	// echo "Hello, " . $post . ". Received Data from _POST";
+	echo "<img src='sample_images/" . $post . ".png'>";
 } else {
 	echo "Hello, I didn't received any data.";
 }
